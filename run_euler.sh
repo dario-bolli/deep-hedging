943a987d8a506d4f6514cb1c28dacf38b2f81159
guillaume <gneven@ethz.ch>
2022-01-05 16:35:09 +0100

    euler script for tuning

--- run_euler.sh
+++ run_euler.sh
@@ -4,11 +4,24 @@
 # pip install QuantLib
 # Then you need to make the file executable like this
 # chmod +x main_Deep_Heedging.py
+# chmod +x run_euler.sh
 module load gcc/6.3.0 python_gpu/3.8.5 eth_proxy
 
 source myenv/bin/activate
 
 # If you want more info about available options of the main, do python ./main_Deep_Heedging.py -h
-bsub -G "s_stud_infk" -n 4 -W 08:00\
-     -R "rusage[mem=4800]" -R "rusage[ngpus_excl_p=1]" \
-      python main_Deep_Heedging.py --N 93 --d 2 --maxT 10 --epochs 80
+
+for d in [5,10,15,30]
+do
+  for m in [1,2]
+  do
+    for maxT in [1, 3, 6, 11,30]
+    do
+      bsub -G "s_stud_infk" -n 4 -W 08:00\
+           -R "rusage[mem=4800]" -R "rusage[ngpus_excl_p=1]" \
+            python main_Deep_Heedging.py --d d --maxT maxT \
+             --epochs 80 --model Deep_Hedging_Model_Transformer \
+             --m m --outdir /deep-hedging/out/
+    done
+  done
+done
