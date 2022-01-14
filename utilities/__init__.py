3b45ac2bcd04c62a24bfb31cba303b74511f50b7
dbolli <dbolli@student.ethz.ch>
2022-01-14 17:56:23 +0000

    Update __init__.py

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,4 +1,5 @@
 from .train_test_split import train_test_split
+from .EuropeanCall import EuropeanCall
 from .BlackScholesProcess import BlackScholesProcess
 from .entropy import Entropy
 from .cvar import CVaR
